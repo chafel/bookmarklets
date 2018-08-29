d3a72a57c75ff462236f8c3294c30b8e41999793
sunchaofei <sunchaofei@didichuxing.com>
2018-08-29 21:00:17 +0800

    feat: implement first version page

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
-# bookmarklets
-A collection of bookmarklets.
+## Bookmarklets
+
+✨ A collection of [bookmarklet](https://en.wikipedia.org/wiki/Bookmarklet) for you.
+❤️ Please help to complement this collection with your brilliant mind.
+
+
+> Make bookmarklet your with [this tool](https://mrcoles.com/bookmarklet/)
---
+++ webpack/webpack.config.prod.js
@@ -0,0 +1,48 @@
+'use strict';
+
+const Path = require('path');
+const Webpack = require('webpack');
+const merge = require('webpack-merge');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const common = require('./webpack.common.js');
+
+module.exports = merge(common, {
+  mode: 'production',
+  devtool: 'source-map',
+  stats: 'errors-only',
+  optimization: {
+    minimize: true
+  },
+  plugins: [
+    new Webpack.DefinePlugin({
+      'process.env.NODE_ENV': JSON.stringify('production')
+    }),
+    // compiling mode “scope hoisting”
+    new Webpack.optimize.ModuleConcatenationPlugin(),
+    new MiniCssExtractPlugin({
+      filename: 'bundle.css'
+    })
+  ],
+  resolve: {
+    alias: {
+      '~': Path.resolve(__dirname, '../src')
+    }
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js)$/,
+        exclude: /(node_modules|bower_components)/,
+        use: 'babel-loader'
+      },
+      {
+        test: /\.s?css/i,
+        use : [
+          MiniCssExtractPlugin.loader,
+          'css-loader',
+          'sass-loader'
+        ]
+      }
+    ]
+  }
+});
